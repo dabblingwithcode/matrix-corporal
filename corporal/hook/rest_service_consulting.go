dabblingwithcode/matrix-corporal

commit 9a878d92edd876d7a9570aaebae8773dae4c7b8a

Fix some golangci-lint-reported errors

corporal/hook/rest_service_consulting.go
@@ -107,7 +107,12 @@ func (me *RESTServiceConsultor) Consult(request *http.Request, response *http.Re
 	if hook.RESTServiceAsync {
 		// We do the same thing we do synchronously. We just do it in the background and don't care what happens.
 		// Still, logging, etc., is done.
-		go me.callRestServiceWithRetries(consultingHTTPRequestFactory, hook, logger)
+		go func() {
+			_, err := me.callRestServiceWithRetries(consultingHTTPRequestFactory, hook, logger)
+			if err != nil {
+				logger.Warnf("Async REST service suffered an error: %s", err)
+			}
+		}()
 
 		if hook.RESTServiceAsyncResultHook != nil {
 			return hook.RESTServiceAsyncResultHook, nil
@@ -221,7 +226,7 @@ func prepareConsultingHTTPRequestFactory(
 	defaultTimeoutDuration time.Duration,
 ) (httpRequestFactory, error) {
 	if hook.RESTServiceURL == nil || *hook.RESTServiceURL == "" {
-		return nil, fmt.Errorf("Cannot use NewRESTServiceConsultor with an empty RESTServiceURL")
+		return nil, fmt.Errorf("cannot use NewRESTServiceConsultor with an empty RESTServiceURL")
 	}
 
 	// We extract the payload once, when making the factory, because that's when we know it's there
@@ -233,12 +238,12 @@ func prepareConsultingHTTPRequestFactory(
 	// if we do it later on for `RESTServiceAsync = true` requests.
 	consultingRequestPayload, err := prepareConsultingHTTPRequestPayload(request, response, hook)
 	if err != nil {
-		return nil, fmt.Errorf("Could not prepare request payload to be sent to the REST service: %s", err)
+		return nil, fmt.Errorf("could not prepare request payload to be sent to the REST service: %s", err)
 	}
 
 	consultingRequestPayloadBytes, err := json.Marshal(consultingRequestPayload)
 	if err != nil {
-		return nil, fmt.Errorf("Could not serialize request payload to be sent to the REST service: %s", err)
+		return nil, fmt.Errorf("could not serialize request payload to be sent to the REST service: %s", err)
 	}
 
 	consultingRequestMethod := "POST"
@@ -253,7 +258,8 @@ func prepareConsultingHTTPRequestFactory(
 
 	return func() (*http.Request, error) {
 		// This needs to be done each time, because it uses absolute time inside.
-		ctx, _ := context.WithTimeout(context.Background(), timeoutDuration)
+		ctx, cancel := context.WithTimeout(context.Background(), timeoutDuration)
+		defer cancel()
 
 		consultingHTTPRequest, err := http.NewRequestWithContext(
 			ctx,

corporal/configuration/configuration.go
@@ -130,7 +130,7 @@ func validateConfiguration(configuration *Configuration, logger *logrus.Logger) 
 			configuration.Matrix.TimeoutMilliseconds,
 		)
 	}
-	if configuration.HttpGateway.InternalRESTAuth.Enabled == nil || (*configuration.HttpGateway.InternalRESTAuth.Enabled) == false {
+	if configuration.HttpGateway.InternalRESTAuth.Enabled == nil || !(*configuration.HttpGateway.InternalRESTAuth.Enabled) {
 		logger.Warn("HttpGateway.InternalRESTAuth.Enabled is neither explicitly enabled, nor disabled. Interactive Auth may not work without it. Define it as enabled or disabled to get rid of this warning")
 	} else {
 		if configuration.HttpGateway.InternalRESTAuth.IPNetworkWhitelist == nil {

corporal/connector/access_token_context.go
@@ -1,6 +1,7 @@
 package connector
 
 import (
+	"log"
 	"sync"
 	"time"
 )
@@ -74,7 +75,10 @@ func (me *AccessTokenContext) Release() {
 	me.userIdToAccessTokenMap.Range(func(userId interface{}, accessTokenInterface interface{}) bool {
 		accessToken := accessTokenInterface.(*AccessToken)
 
-		me.connector.DestroyAccessToken(userId.(string), accessToken.Token())
+		err := me.connector.DestroyAccessToken(userId.(string), accessToken.Token())
+		if err != nil {
+			log.Printf("failed destroying access token for user %s: %s", userId.(string), err)
+		}
 
 		me.userIdToAccessTokenMap.Delete(userId)
 

corporal/connector/api.go
@@ -444,7 +444,10 @@ func (me *ApiConnector) DemoteUserInRoom(
 		return nil
 	}
 
-	jsonObj.Set(0, "users", userId)
+	_, err = jsonObj.Set(0, "users", userId)
+	if err != nil {
+		return fmt.Errorf("failed setting user in power levels object: %s", err)
+	}
 
 	return matrix.ExecuteWithRateLimitRetries(me.logger, "user.demote", func() error {
 		_, err := client.SendStateEvent(roomId, "m.room.power_levels", "", jsonObj.Data())

corporal/connector/synapse.go
@@ -253,7 +253,10 @@ func (me *SynapseConnector) EnsureUserAccountExists(userId, password string) err
 	// The register API creates an access token automatically.
 	// We don't need it and we'd rather be nice and get rid of it, to keep things clean.
 	clientForUser, _ := gomatrix.NewClient(me.homeserverApiEndpoint, userIdLocalPart, registerResponse.AccessToken)
-	clientForUser.Logout()
+	_, err = clientForUser.Logout()
+	if err != nil {
+		me.logger.Warnf("failed logging out user %s: %s", userIdLocalPart, err)
+	}
 
 	return nil
 }

corporal/container/container.go
@@ -38,7 +38,7 @@ func (me *ContainerShutdownHandler) Add(destructor func()) {
 }
 
 func (me *ContainerShutdownHandler) Shutdown() {
-	for i, _ := range me.destructors {
+	for i := range me.destructors {
 		me.destructors[len(me.destructors)-i-1]()
 	}
 }
@@ -125,7 +125,10 @@ func BuildContainer(
 		)
 
 		shutdownHandler.Add(func() {
-			instance.Stop()
+			err := instance.Stop()
+			if err != nil {
+				logger.Errorf("failed stopping HTTP gateway server: %s", err)
+			}
 		})
 
 		return instance
@@ -195,7 +198,10 @@ func BuildContainer(
 		)
 
 		shutdownHandler.Add(func() {
-			instance.Stop()
+			err := instance.Stop()
+			if err != nil {
+				logger.Errorf("failed stopping HTTP API server: %s", err)
+			}
 		})
 
 		return instance

corporal/httpapi/handler/common.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"devture-matrix-corporal/corporal/httphelp"
 	"devture-matrix-corporal/corporal/matrix"
 	"encoding/json"
 	"fmt"
@@ -24,13 +25,10 @@ type ApiResponseError struct {
 }
 
 func Respond(w http.ResponseWriter, httpStatusCode int, resp interface{}) {
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(httpStatusCode)
-
 	respBytes, err := json.Marshal(resp)
 	if err != nil {
-		panic(fmt.Errorf("Could not create JSON response for: %s", resp))
+		panic(fmt.Errorf("could not create JSON response for: %s", resp))
 	}
 
-	w.Write(respBytes)
+	httphelp.RespondWithBytes(w, httpStatusCode, "application/json", respBytes)
 }

corporal/httpapi/server.go
@@ -64,9 +64,7 @@ func (me *Server) Stop() error {
 	}
 
 	me.logger.Infoln("Stopping HTTP API Server")
-	me.server.Shutdown(context.Background())
-
-	return nil
+	return me.server.Shutdown(context.Background())
 }
 
 func (me *Server) createRouter() http.Handler {

commit 0c27562c2d1c621c251532054f6b358696d3495f

Fix context-cancellation regression affecting REST hooks
Fixes a regression introduced in 9a878d92edd876d7a9570aaebae8773dae4c7b8a

The previous code was working, but not ideal, because context
cancellation was no handled at all.
Ref: https://pkg.go.dev/golang.org/x/tools/go/analysis/passes/lostcancel

Fixes https://github.com/devture/matrix-corporal/issues/26

corporal/hook/rest_service_consulting.go
@@ -14,7 +14,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type httpRequestFactory func() (*http.Request, error)
+type httpRequestFactory func() (*http.Request, *context.CancelFunc, error)
 
 // restServiceConsultingRequest reprents as request payload to be sent to a REST service.
 //
@@ -149,11 +149,12 @@ func (me *RESTServiceConsultor) callRestServiceWithRetries(
 	var restError error
 
 	for attemptNumber := uint(1); attemptNumber <= attemptsCount; attemptNumber++ {
-		requestToSend, err := requestFactory()
+		requestToSend, cancel, err := requestFactory()
 		if err != nil {
 			logger.Errorf("RESTServiceConsultor: failed preparing HTTP Request: %s", err)
 			return nil, err
 		}
+		defer (*cancel)()
 
 		logger = logger.WithFields(logrus.Fields{
 			"RESTRrequestMethod": requestToSend.Method,
@@ -256,10 +257,9 @@ func prepareConsultingHTTPRequestFactory(
 		timeoutDuration = time.Duration(*hook.RESTServiceRequestTimeoutMilliseconds) * time.Millisecond
 	}
 
-	return func() (*http.Request, error) {
+	return func() (*http.Request, *context.CancelFunc, error) {
 		// This needs to be done each time, because it uses absolute time inside.
 		ctx, cancel := context.WithTimeout(context.Background(), timeoutDuration)
-		defer cancel()
 
 		consultingHTTPRequest, err := http.NewRequestWithContext(
 			ctx,
@@ -268,7 +268,8 @@ func prepareConsultingHTTPRequestFactory(
 			bytes.NewReader(consultingRequestPayloadBytes),
 		)
 		if err != nil {
-			return nil, err
+			cancel()
+			return nil, nil, err
 		}
 
 		consultingHTTPRequest.Header.Set("Content-Type", "application/json")
@@ -278,7 +279,7 @@ func prepareConsultingHTTPRequestFactory(
 			}
 		}
 
-		return consultingHTTPRequest, nil
+		return consultingHTTPRequest, &cancel, nil
 	}, nil
 }
 